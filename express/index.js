e1d41bcd3c15b952f29c58864de683c3ef8aae50
gopallchoudhary <gopalchoudhary521@gmail.com>
2025-12-10 10:38:28 +0530

    dockerized express app

---
+++ express/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("Hello World from dockerized express app")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+    
+})
